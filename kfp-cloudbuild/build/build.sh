273a47129c797cf0d742d363bf3dabd6a6a5cd7e
Cesar <cesar_ipn2@hotmail.com>
2021-03-07 14:02:11 -0600

    eliminage gs

--- kfp-cloudbuild/build/build.sh
+++ kfp-cloudbuild/build/build.sh
@@ -23,7 +23,6 @@ _REPO_URL='https://github.com/cesarwordbox0205/kubeflow-examples.git',\
 _PROJECT_ID='youtubelist-256522',\
 _COMPUTE_ZONE='us-central1-a',\
 _CLUSTER_NAME='cluster-1',\
-_GCS_LOCATION='youtubelist-256522-kubeflowpipelines-default/pipelines',\
 _EXPERIMENT_NAME='helloworld-dev',\
 _TAG='latest'
 
